19d336a850549f4f2e27514aa089b5cadb2d1fa8
mgarcia24 <garciamaira524@yahoo.com>
2020-04-28 15:26:45 -0700

    modified some features and README file

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Code Quiz Project
+
+This application is a timed quiz about random subjects, using JavaScript to dynamically add, modify, and hide HTML elements and CSS styling. 
+
+When the user goes to the site, he/she/they will be able to click on the `start quiz` button, where a user will be able to click on the answer for each question. The score will be calculated at the end of the quiz.
+
+On the home page, the user will also be able to access his/her/their past scores, using the `view high scores` button. 
+
+During the quiz, this button will be disabled, so that the user is able to fully concentrate on the quiz. 
+
+## Quiz Elements
+
+As mentioned above, the user will have to click the best answer for each question to move on to the next question with its corresponding answer choices. There are 4 choices per question. 
+
+The questions and corresponding answers are rotated using a for-loop. 
+
+## Quiz Finished
+
+When the user finishes answering all the questions, or when the timer goes down to `0`, the quiz will end and an input form will display.  The user will type his/her/their initials and press the `'submit` button. Should the user fail to insert the initials, an `alert` will display, prompting the user to type in the initials before advancing further. 
+
+## Score Board
+
+After, the user is taken to another page where he/she/they are able to view the scores he/she/they earned in the quiz. In that page, the user is able to either click the `start over` button to take the quiz, or the `clear leaderboard` button to clear all the scores saved. 
--- script.js
+++ script.js
@@ -1,31 +1,3 @@
-// # 04 Web APIs: Code Quiz
-
-// As you proceed in your career as a web developer, you will probably be asked to complete a coding assessment, which is typically a combination of multiple-choice questions and interactive challenges. Build a timed code quiz with multiple-choice questions. This app will run in the browser and feature dynamically updated HTML and CSS powered by your JavaScript code. It will also feature a clean and polished user interface and be responsive, ensuring that it adapts to multiple screen sizes.
-
-// ## User Story
-
-// ```
-// AS A coding bootcamp student
-// I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
-// SO THAT I can gauge my progress compared to my peers
-// ```
-
-// ## Acceptance Criteria
-
-// ```
-// GIVEN I am taking a code quiz
-// WHEN I click the start button
-// THEN a timer starts and I am presented with a question
-// WHEN I answer a question
-// THEN I am presented with another question
-// WHEN I answer a question incorrectly
-// THEN time is subtracted from the clock
-// WHEN all questions are answered or the timer reaches 0
-// THEN the game is over
-// WHEN the game is over
-// THEN I can save my initials and score
-// ```
-
 // Quiz with questions, options, and answers
 var quiz = [{"index": 0, "question": "What are the building blocks of the web?",
 "choices": ["Ruby, Python, HTML", "JavaScript, jQuery, Bootstrap", "HTML, CSS, JavaScript", "Variables, C++, jQuery"],
@@ -72,7 +44,7 @@ choices: ["Caillou", "TeleTubbies", "Sesame Street", "Mr. Roger's Neighborhood"]
 answer: 2
 },
 
-{"index": 9, "question":  "What is the world's highest mountain",
+{"index": 9, "question":  "What is the world's highest mountain?",
 choices: ["Kilimanjaro", "K2", "Mount Everest", "Makalu"],
 answer: 2
 },
@@ -88,7 +60,7 @@ answer: 1
 },
 
 {"index": 12, "question": "Which of the following are NOT part of the 7 new wonders of the world? ",
-choices: ["Machu Picchu", "Christ the Redeemer", "Great Pyramid of Giza,", "Taj Mahal"],
+choices: ["Machu Picchu", "Christ the Redeemer", "Great Pyramid of Giza", "Taj Mahal"],
 answer: 2
 },
 
